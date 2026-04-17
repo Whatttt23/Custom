7f6431733bb8947fe917277979dd0c13e2afbe0d
Whatttt23 <rjjdkskssososksk@gmail.com>
2026-04-17 11:53:58 +0700

    Update image loader source URL in Example.lua

---
+++ Example.lua
@@ -0,0 +1,8 @@
+local load = loadstring(game:HttpGet("https://raw.githubusercontent.com/Whatttt23/Custom-Image/main/Source.lua"))()
+
+local img = load:Create({
+    Image = "https://github.com/xxx/image.png",
+    Parent = game.CoreGui,
+    Size = UDim2.new(0,200,0,200),
+    Position = UDim2.new(0.5,-100,0.5,-100)
+})
