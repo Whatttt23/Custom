29676acd1a6bf31c9ec3538d32090078f0cac8f8
Whatttt23 <rjjdkskssososksk@gmail.com>
2026-04-17 12:00:31 +0700

    Update Example.lua

--- Image/Example.lua
+++ Image/Example.lua
@@ -5,4 +5,5 @@ local img = load:Create({
     Parent = game.CoreGui,
     Size = UDim2.new(0,200,0,200),
     Position = UDim2.new(0.5,-100,0.5,-100)
+    ... -- The rest are similar to the functions in Roblox
 })
